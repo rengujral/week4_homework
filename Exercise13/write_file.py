6077468f98d89e901ccde1c24eed6463415207b2
Renee Gujral <reneegujral1@gmail.com>
2025-02-18 10:13:35 +0000

    EDIT: added comments from renee_week4_homework to write_file.py

--- Exercise13/write_file.py
+++ Exercise13/write_file.py
@@ -1,6 +1,15 @@
+# the open function opens the specified .txt file
+# 'a' (append mode) allows adding new content to the file without overwriting existing content
+file_handle = open("pelican.txt", 'a')
 
-with open ("pelican.txt", 'a') as file_handle:
-    file_handle.write("A wonderful bird is the pelican\n")
-    file_handle.write("His bill holds more than his belican\n")
-    lines = ["He can take in his beak, \n", "Enough food for a week, \n", "But I'm damned if I see how the helican. \n"]
-    file_handle.writelines(lines)
+# .write() method writes one string to the file
+file_handle.write("A wonderful bird is the pelican\n")
+
+file_handle.write("His bill holds more than his belican\n")
+
+# Create a list of strings, with multiple lines of poem
+# \n (new line) separates each string in the list
+lines = ["He can take in his beak,\n", "Enough food for a week,\n", "But I'm damned if I see how the helican."]
+# .writelines method writes multiple lines from a list to the file
+# .writelines assumes each line is a string
+file_handle.writelines(lines)
